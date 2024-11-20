42f764a450b1713fc25e610315312e9f65057d5f
Andrzej Borzym <borzym.a@gmail.com>
2024-11-20 22:52:41 +0100

    searchBar styles done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -23,19 +23,31 @@ const SearchBar = ({ onSubmit }) => {
 
   return (
     <header className={css.header}>
-      <form onSubmit={handleSubmit}>
-        <input
-          className={css.input}
-          type="text"
-          name="query"
-          value={query}
-          onChange={handleChange}
-          autoComplete="off"
-          autoFocus
-          placeholder="Search images and photos"
-        />
-        <button type="submit">Search</button>
-      </form>
+      <div className={css.inputWrapper}>
+        <form onSubmit={handleSubmit} className={css.searchForm}>
+          <input
+            className={css.input}
+            type="text"
+            name="query"
+            value={query}
+            onChange={handleChange}
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          />
+          <button type="submit" className={css.searchButton}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.099zm-5.442 1.207a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11z" />
+            </svg>
+          </button>
+        </form>
+      </div>
       <Toaster />
     </header>
   );
